cd7cfe4cf888541cbd802c1ae26de08175d0206f
NexZen Foundation <info@nexzenfoundation.in>
2026-05-24 19:49:18 +0530

    fix: mobile donate button gap, side content clipping, Razorpay amount pre-fill

--- src/app/donate/page.js
+++ src/app/donate/page.js
@@ -127,7 +127,7 @@ export default function Donate() {
                 )}
               </div>
 
-              <RazorpayButton />
+              <RazorpayButton amount={selectedAmount} />
             </div>
 
             {/* Bank Transfer */}
